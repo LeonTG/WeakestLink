f845844e33f5b4f6d47d2f9655a6cd7d4ec5a792
LeonTG77 <leontg77@gmail.com>
2016-05-03 09:28:23 +0200

    Added broadcast(String)

--- src/com/leontg77/weakestlink/Main.java
+++ src/com/leontg77/weakestlink/Main.java
@@ -1,9 +1,11 @@
 package com.leontg77.weakestlink;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.leontg77.weakestlink.cmds.WeakestLinkCommand;
+import com.leontg77.weakestlink.commands.WeakestLinkCommand;
 
 /**
  * Main class of the plugin.
@@ -11,30 +13,38 @@ import com.leontg77.weakestlink.cmds.WeakestLinkCommand;
  * @author LeonTG77
  */
 public class Main extends JavaPlugin {
-	public static final String PREFIX = "PREFIX HERE!!! ";
-	public static Main plugin;
+	public static final String PREFIX = "§6Weakest Link §8» §f";
 
 	@Override
 	public void onDisable() {
-		// print a message to the console saying it has been disabled.
 		PluginDescriptionFile file = getDescription();
-		getLogger().info(file.getName() + " has been disabled.");
-		
-		// set the plugin field to null.
-		plugin = null;
+		getLogger().info(file.getName() + " is now disabled.");
 	}
 	
 	@Override
 	public void onEnable() {
-		// print a message to the console saying it has been enabled.
 		PluginDescriptionFile file = getDescription();
-		getLogger().info(file.getName() + " v" + file.getVersion() + " has been enabled.");
+		getLogger().info(file.getName() + " v" + file.getVersion() + " is now enabled.");
 		getLogger().info("Plugin is made by LeonTG77.");
 		
-		// register the /vs command.
-		getCommand("weakestlink").setExecutor(new WeakestLinkCommand());
+		KillingTask task = new KillingTask(this);
+		WeakestLinkCommand cmd = new WeakestLinkCommand(this, task);
 		
-		// set the plugin field to this class.
-		plugin = this;
+		// register command.
+		getCommand("weakestlink").setExecutor(cmd);
+		getCommand("weakestlink").setTabCompleter(cmd);
+	}
+	
+	/**
+	 * Send the given message to all online players.
+	 * 
+	 * @param message The message to send.
+	 */
+	public void broadcast(String message) {
+		for (Player online : Bukkit.getOnlinePlayers()) {
+			online.sendMessage(message);
+		}
+		
+		Bukkit.getLogger().info(message);
 	}
 }
